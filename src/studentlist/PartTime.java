e8fb321e12f685da1ed099eee36048d5cd38a972
sivagamasrinivasan <784108@pdsb.net>
2025-05-20 16:55:41 -0400

    PArt time new class addded

---
+++ src/studentlist/PartTime.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+public class PartTime {
+    private int sid;
+    private String daysname;
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
